f07e8b27a127bcc840997b4180f0a6603505f923
unknown <162107@SNKXW-JBAUGUES2.sn.corp>
2015-04-06 16:36:55 -0400

    First commit

---
+++ client/app.js
@@ -0,0 +1,57 @@
+var app = angular.module('UpdateSetApp', []);
+app.controller('UpdateSetController', ['$scope', '$http', function($scope, $http){
+	$http.get('/updatesets/names')
+		.then(function(data){
+			var dataArr = [];
+			var datum = data.data;
+			for (var item in datum) {
+				dataArr.push(
+					{'name': datum[item].name, 
+					 'description' : datum[item].description, 
+					 'migration_plan' : datum[item].u_migration_plan,
+					 'rally' : datum[item].u_rally,
+					 'closed_by' : datum[item].completed_by,
+					 'closed_on' : datum[item].completed_on, 
+					 'opened_by' : datum[item].sys_created_by, 
+					 'state' : datum[item].state,
+					 'created_on' : datum[item].sys_created_on,
+					 'changeNumber' : ''
+					});
+			}
+			$scope.options = dataArr;
+		});
+	$scope.updateList = function(updateSets){
+		$scope.resultsHolder = updateSets;
+	}
+	$scope.updateChangeNumber = function(){
+		var results = $scope.resultsHolder;
+		for (var result in results){
+			results[result].changeNumber = $scope.change;
+		}
+		$scope.resultsHolder = results;
+	}
+	$scope.updateReleaseNumber = function(){
+		var results = $scope.resultsHolder;
+		for (var result in results){
+			results[result].releaseNumber = $scope.release;
+		}
+		$scope.resultsHolder = results;
+	}
+	$scope.createMigration = function(){		
+		$http.post('/updatesets/migration', $scope.resultsHolder)
+			.then(function(data){
+				console.log(data);
+			});
+	}
+	$scope.updatePassword = function(){
+		var results = $scope.resultsHolder;
+		for (var result in results){
+			results[result].password = $scope.password;
+		}
+		$scope.resultsHolder = results;	
+	}
+}]).directive('resultsHolder', function(){
+	return {
+		templateUrl: 'resultsHolder.html'
+	}
+});
---
+++ server.js
@@ -0,0 +1,79 @@
+var express = require('express'), bodyParser = require('body-parser'), http = require('http'), request = require('request'), Promise = require('promise');
+app = express();
+app.use(bodyParser());
+app.use('/', express.static('./client'));
+var username = process.env.USERNAME;
+var password = process.env.PASSWORD;
+
+var returnRequest = function() {
+	return new Promise(function(resolve, reject){
+	var hostname = "https://scrippsdev.service-now.com/sys_update_set.do?JSONv2&sysparm_action=getRecords&sysparm_query=name!=Default^ORDERBYDESCsys_updated_on&displayvalue=true";
+	request.get(hostname, function(error, response, body){
+		if (error) {
+			reject(error);
+		}
+		var parsed = JSON.parse(body);
+		if (parsed) {
+			var records = parsed.records;
+			resolve(records);
+		} else {
+			console.log("Error parsing JSON");
+		}
+	}).auth(username, password, false);
+});
+}
+
+var createMigration = function(inputData) {
+	return new Promise(function(resolve, reject){
+		if (inputData[0].password != password){
+			reject("Invalid Password");
+		}
+		var hostname = "https://scrippsdev.service-now.com/kb_knowledge.do?JSON&sysparm_action=insert";		
+		var text = '', shortDesc = 'Migration Plan - ' + inputData[0].changeNumber + ' - ServiceNow Release ' + inputData[0].releaseNumber;
+		var tableStart = "<table border='1px solid #000;'><tr><td>Number</td><td>Name</td><td>Description</td><td>State</td><td>Opened By</td><td>Closed By</td><td>Closed On</td><td>Rally</td></tr>";
+		var tableBody = '';
+		var tableEnd = "</table>";
+		for (var input in inputData) {
+			text += "<h1>Update Set: <u>" + inputData[input].name + "</u> Steps</h1><br/>"
+			tableBody += "<tr><td>" + (parseInt(input) + 1) + "</td>" +
+					"<td>" + inputData[input].name + "</td>" +
+					"<td>" + inputData[input].description + "</td>" +
+					"<td>" + inputData[input].state + "</td>" +
+					"<td>" + inputData[input].opened_by + "</td>" +
+					"<td>" + inputData[input].closed_by + "</td>" +
+					"<td>" + inputData[input].closed_on + "</td>" +
+					"<td><a href='" + inputData[input].rally + "'>Link</a></tr>";
+			text += inputData[input].migration_plan + "<br/>"
+		}	
+		var heading = "<h1><a target='_blank' href='https://scripps.service-now.com/change_request.do?sysparm_query=number=" + inputData[0].changeNumber + "'>" + shortDesc + '</a></h1>';
+		var finalText =  heading + tableStart + tableBody + tableEnd + text;
+		var knowledgeArticle = {text : finalText, short_description : shortDesc};
+		var options = {
+			uri : hostname,
+			method : 'POST',
+			json : knowledgeArticle
+		};
+		request(options, function(error, response, body){
+			if (error) {
+				reject(error);
+			}
+			resolve(response);
+		}).auth(username, password, false);
+	});
+}
+
+app.post('/updatesets/migration', function(req, res){
+	var promiseObject = createMigration(req.body)
+		.then(function(response){
+			res.json(response);
+		});
+});
+
+app.get('/updatesets/names', function(req, res){
+	var promiseObject = returnRequest()
+		.then(function(names){
+			res.json(names);
+		});
+});
+
+app.listen(8080);
